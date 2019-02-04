e2019df9032b4b153026245d34989bde365b5829
Grzegorz Szeliga <szeligagrzegorz@gmail.com>
2019-02-04 19:15:27 +0100

    remove comment

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import { observable, autorun, computed } from 'mobx';
 
 import * as snabbdom from 'snabbdom';
 
-/*
 import snabbdomClass from 'snabbdom/modules/class';
 import snabbdomProps from 'snabbdom/modules/props';
 import snabbdomStyle from 'snabbdom/modules/style';
@@ -15,16 +14,6 @@ var patch = snabbdom.init([
     snabbdomStyle,
     snabbdomEventlisteners,
 ]);
-*/
-
-const patch = snabbdom.init([ // Init patch function with chosen modules
-    require('snabbdom/modules/class').default, // makes it easy to toggle classes
-    require('snabbdom/modules/props').default, // for setting properties on DOM elements
-    require('snabbdom/modules/style').default, // handles styling on elements with support for animations
-    require('snabbdom/modules/eventlisteners').default, // attaches event listeners
-    require('snabbdom/modules/attributes').default,
-]);
-const h = require('snabbdom/h').default;
 
 class AppState {
     @observable counter: number;
@@ -54,8 +43,10 @@ const renderView = (appState: AppState) => {
     const nowyKolor = appState.show ? 'red' : 'blue';
     const nowyKolor2 = appState.even ? 'green' : 'orange';
 
-    console.info('Nowy kolor', nowyKolor);
-    console.info('appState.even', appState.even);
+    console.info({
+        'Nowy kolor': nowyKolor,
+        'appState.even': appState.even
+    });
 
     return h('div', {on: {click: appState.toogle}}, [
         h('span', {style: {color: nowyKolor}}, 'This is bold'),
